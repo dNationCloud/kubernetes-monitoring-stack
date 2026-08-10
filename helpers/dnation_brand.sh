0ca6fb3e5bd6578226f5ff2102248f5c13bc1573
Viktor Cech <viktor.cech@dnation.cloud>
2026-08-10 16:35:36 +0200

    Upgrade monitoring stack to Grafana 13.1
    Signed-off-by: Viktor Cech <viktor.cech@dnation.cloud>

--- helpers/dnation_brand.sh
+++ helpers/dnation_brand.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 # Modify favicon
 sed -i 's/\[\[.AppTitle\]\]/dNation/g' /usr/share/grafana/public/views/index.html
-sed -i 's/\[\[.AppTitle\]\]/dNation/g' /usr/share/grafana/public/views/index-template.html
 sed -i 's/\[\[.FavIcon\]\]/https:\/\/storage.googleapis.com\/cdn.ifne.eu\/public\/icons\/dnation_k8sm8g.png/g' /usr/share/grafana/public/views/index.html
 #Modify logp
 sed -i 's@public/img/grafana_icon.svg@public/img/dnation_logo.svg@g' /usr/share/grafana/public/build/*.*
+sed -i 's@static/img/grafana_icon\.[a-f0-9]*\.svg@../img/dnation_logo.svg@g' /usr/share/grafana/public/build/*.js
 #Modify app title
 sed -i 's/"AppTitle","Grafana"/"AppTitle","dNation"/g' /usr/share/grafana/public/build/*.*
 sed -i "s/static AppTitle = 'Grafana'/static AppTitle = 'dNation'/g" /usr/share/grafana/public/build/*.*
+sed -i 's/AppTitle="Grafana"/AppTitle="dNation"/g' /usr/share/grafana/public/build/*.js
 #Modify welcome message
 sed -i 's/Welcome to Grafana/dNation Kubernetes Monitoring/g' /usr/share/grafana/public/build/*.*
 #Modify the docs
